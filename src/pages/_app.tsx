8639d4c584446c6f248b251effab84e884f51bee
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-01-14 16:54:08 -0500

    Some More cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,7 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   LedgerWalletAdapter,
   PhantomWalletAdapter,
@@ -18,12 +15,10 @@ import {
 import { clusterApiUrl } from "@solana/web3.js";
 import { AppProps } from "next/app";
 import { FC, useMemo } from "react";
-// Use require instead of import since order matters
+
 require("@solana/wallet-adapter-react-ui/styles.css");
-import { BiArrowBack } from "react-icons/bi";
 
 import Navbar from "@/components/Navbar";
-import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { wallet, publicKey } = useWallet();
